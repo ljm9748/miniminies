d9ae9b990ccdb3438d15a0bbeea8d53ed2136772
Jungmin <ljm9748@naver.com>
2020-10-30 15:23:59 +0900

    Update TestMain.java

--- Miniminigames/src/firstproject/TestMain.java
+++ Miniminigames/src/firstproject/TestMain.java
@@ -4,6 +4,9 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("여러분안녕하세요! 미니미니게임즈 입니다");
+		System.out.println("git branch test");
 
 	}
 
