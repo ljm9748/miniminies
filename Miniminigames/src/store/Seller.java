5517f588c11aeb206144e6f9ea95acb183a7b931
JeongSuIn <suin9711@naver.com>
2020-11-02 18:40:32 +0900

    store
    store

--- Miniminigames/src/store/Seller.java
+++ Miniminigames/src/store/Seller.java
@@ -1,34 +1,62 @@
 package store;
 
 public class Seller {
+
 	
-	// 20.10.30 추가
-	// 변수 설정
-	int point;		// 포인트 // 초기화 = player가 현재 보유하고 있는 포인트
-	int money;		// 보유 금액 // 
-	private int LIFE_PRICE;	// 라이프 금액
-	
-	
-	// 생성자 추가
-	Seller(int myPoint, int myMoney){
-		this.point = myPoint;
-		this.money = myMoney;
-	}
-	
-	// 라이프 판매 메소드
-	int saleLife(int myMoney) {
-		int numOfLife = 0; // 0 -> 현재 player가 보유하고 있는 라이프 개수
+	// 클래스에는 변수와 메소드를 정의
+		// 성격이 비슷한 변수, 메소드를 정의한다.		
 		
-		// 반환할 라이프 개수를 구한다.
-		numOfLife = myMoney/LIFE_PRICE;
-		// 수익금 증가
-		money = money + myMoney;
-		// 라이프 보유 개수 -> 무제한일 경우에는....?
+		final int LIFE_PRICE; 	 				// 상수 처리 - 라이프의 가격, 변경이 불가능
+		private int numOfApple; 			 	// 사과의 보유 개수
+		private int myMoney;				 	// 수익 금액
+		
+		//2020.10.08 생성자 추가
+		Seller(){ // 기본 생성자
+			this(0, 20, 1000); // 생성자 FruitSeller를 가리킴.
+		}
+		
+		// 생성자의 오버로딩
+		Seller(int money, int num, int price){
+			APPLE_PRICE=price;
+			numOfApple = num;
+			myMoney = money;
+		}
+		
+		Seller(Seller seller){
+//			APPLE_PRICE = seller.APPLE_PRICE;
+//			numOfApple = seller.numOfApple;
+//			myMoney = seller.myMoney;
+			this(seller.myMoney, seller.numOfApple, seller.APPLE_PRICE);
+		}
 		
 		
-		return numOfLife;
-	}
-	
-	
-	
+		// 기능: 판매, 수익금을 정산 출력
+		// 판매 메소드:
+		//	돈을 받고  
+		// 		-> 반환하는 사과의 개수,
+		//		수익금 증가,
+		//		사과 보유 개수가 감소  
+		// 	-> 사과의 개수 반환
+		int saleApple(int money) {
+			
+			int num = 0; 		//반환할 사과의 개수, 지역 변수는 반드시 초기화해주어야 한다. 
+			
+			// 반환할 사과의 개수를 구한다.
+			num = money/APPLE_PRICE; 		// 받은 돈/사과의 가격
+			// 수익금 증가
+			myMoney = myMoney + money;
+			// 사과 보유 개수 감소
+			numOfApple = numOfApple - num;
+			
+			
+			return num;
+		}
+		
+		// 정산 출력: 보유한 사과의 개수 출력, 보유한 금액 출력, 
+		// 받는 데이터가 없으므로 매개변수 정의하지 않는다.
+		// 반환하는 데이터가 없으므로 반환타입을 void 타입으로 메소드 정의
+		void showSaleResult() { 	// 반환 타입이 x
+			System.out.println("현재 보유한 사과의 개수: " + numOfApple);
+			System.out.println("판매 수익 금액: " + myMoney );
+		}
 }
