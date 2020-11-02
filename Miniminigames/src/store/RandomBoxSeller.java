21d08974888d47fa45b6995a65d900c521fc743b
JeongSuIn <suin9711@naver.com>
2020-11-02 20:07:21 +0900

    store
    store

---
+++ Miniminigames/src/store/RandomBoxSeller.java
@@ -0,0 +1,63 @@
+package store;
+
+public class RandomBoxSeller {
+
+	// 클래스에는 변수와 메소드를 정의
+			// 성격이 비슷한 변수, 메소드를 정의한다.		
+			
+			final int RANDOMBOX_PRICE; 	 			// 상수 처리 - 랜덤박스의 가격, 변경이 불가능
+			private int numOfRandomBox;
+			private int getPoint; 				 	// getPoint의 수
+			private int myPoint;				 	// 보유 포인트 // 잔액 불러오기
+			private int myMoney;					// 보유 금액
+			
+			
+			RandomBoxSeller(){ // 기본 생성자
+				this(0, 0, 50); 
+			}
+			
+			// 생성자의 오버로딩
+			RandomBoxSeller(int point, int getPoint, int price){
+				RANDOMBOX_PRICE = price;
+				this.getPoint = getPoint;
+				myPoint = point;
+			}
+			
+			RandomBoxSeller(RandomBoxSeller randomboxseller){		
+				this(randomboxseller.myPoint, randomboxseller.getPoint, randomboxseller.RANDOMBOX_PRICE);
+			}
+			
+			
+			// 기능: 판매, 수익금을 정산 출력
+			// 판매 메소드:
+			//	돈을 받고  
+			// 		-> 반환하는 사과의 개수,
+			//		수익금 증가,
+			//		사과 보유 개수가 감소  
+			// 	-> 사과의 개수 반환
+			
+			 //반환할 라이프의 개수, 지역 변수는 반드시 초기화해주어야 한다. 
+			int saleRandomBox(int point) {
+				
+				// 반환할  랜덤 포인트의 개수를 구한다.				
+				getPoint = (int)(Math.random()*20)*10; 		// 랜덤으로 포인트 지급 -> (1~20)*10 -> 10~200 지급
+				// 수익금 증가
+				myPoint = myPoint + point;
+				// money로 환전
+				myMoney = myPoint;
+				// 판매한 랜덤박스 수량
+				numOfRandomBox = point/RANDOMBOX_PRICE;
+				
+				return getPoint;
+			}
+			
+			// 정산 출력: 보유한 사과의 개수 출력, 보유한 금액 출력, 
+			// 받는 데이터가 없으므로 매개변수 정의하지 않는다.
+			// 반환하는 데이터가 없으므로 반환타입을 void 타입으로 메소드 정의
+			void showSaleRandomBoxResult() { 	// 반환 타입이 x
+				System.out.println("판매한 라이프의 개수 >> " + numOfRandomBox);
+				System.out.println("판매 수익 금액 >> " + myPoint );
+				System.out.println("보유 금액 >> " + myMoney);
+			}
+	
+}
--- Miniminigames/src/store/SaleMain.java
+++ Miniminigames/src/store/SaleMain.java
@@ -8,46 +8,91 @@ public class SaleMain {
 	
 	public static void main(String[] args) {
 		
+			while(true) {
 		
-
-			// LifeSeller 타입의 참조변수 선언
-			LifeSeller lifeseller = null;
-			// LifeSeller의 객체 생성
-			// 객체의 생성은 클래스에 정의된 멤버(변수, 메서드)들을 메모리에 등록하는 것.
+				System.out.println("*****메뉴를 입력하세요*****");
+				System.out.println("1. 라이프 충전");
+				System.out.println("2. 랜덤박스 구매");
+				System.out.println("3. 포인트 충전");
 			
-			// 셀러1
-			lifeseller = new LifeSeller(0, 30, 100); // 객체 생성 후 객체의 주소값을 반환
-								//	point, numLife, price 
+				
+				
+				int select = 0;
+				select = sc.nextInt();
 			
-						
-			// LifeBuyer 타입의 참조변수 선언과 초기화
-			LifeBuyer lifebuyer = new LifeBuyer(1000, 0); // 인스턴스 생성
-										// 현재잔액 포인트, 라이프 개수
+				switch(select) {
+	
+				case 1:
+					// LifeSeller 타입의 참조변수 선언
+					LifeSeller lifeseller = null;
+					// LifeSeller의 객체 생성
+					// 객체의 생성은 클래스에 정의된 멤버(변수, 메서드)들을 메모리에 등록하는 것.
+					
+					// 셀러1
+					lifeseller = new LifeSeller(0, 30, 100); // 객체 생성 후 객체의 주소값을 반환
+										//	point, numLife, price 
+					
 								
-			// 라이프 구매: seller 1에게 원하는 값 지급
-			int givePoint = 0;			
-			System.out.println("구매할 라이프의 개수를 입력하세요.");	
-			givePoint = sc.nextInt();
-			lifebuyer.buyLife(lifeseller, givePoint*100);
-			
-			
-			
-			//20.10.15(금) - 제어자 추가 -> 정보 은닉
-//			seller.myMoney += 500;
-//			buyer.myMoney -= 500;
-//			
-//			seller.numOfApple -= 20;
-//			buyer.numOfApple += 20;
-									
-			System.out.println("\n# 라이프 판매자의 현재 지표\n");
-			lifeseller.showSaleLifeResult();
-			
-			System.out.println("----------------------------------");
-			
-						
-			System.out.println("\n# 라이프 구매자의 현재 지표\n");
-			lifebuyer.showBuyLifeResult();
-
+					// LifeBuyer 타입의 참조변수 선언과 초기화
+					LifeBuyer lifebuyer = new LifeBuyer(1000, 0); // 인스턴스 생성
+												// 현재잔액 포인트, 라이프 개수
+										
+					// 라이프 구매: seller 1에게 원하는 값 지급
+					int givePoint = 0;			
+					System.out.println("구매할 라이프의 개수를 입력하세요.");	
+					givePoint = sc.nextInt();
+					lifebuyer.buyLife(lifeseller, givePoint*100);
+															
+					System.out.println("\n# 라이프 판매자의 현재 지표\n");
+					lifeseller.showSaleLifeResult();
+					
+					System.out.println("----------------------------------");
+					
+								
+					System.out.println("\n# 라이프 구매자의 현재 지표\n");
+					lifebuyer.showBuyLifeResult();
+					
+					break;
+					
+				case 2:
+					// RandomBoxSeller 타입의 참조변수 선언
+					RandomBoxSeller randomboxseller = null;
+					// LifeSeller의 객체 생성
+					// 객체의 생성은 클래스에 정의된 멤버(변수, 메서드)들을 메모리에 등록하는 것.
+					
+					// 랜덤박스 셀러
+					randomboxseller = new RandomBoxSeller(0, 0, 50); // 객체 생성 후 객체의 주소값을 반환
+										//	point, getPoint, price 
+					
+								
+					// RandomBoxBuyer 타입의 참조변수 선언과 초기화
+					RandomBoxBuyer randomboxbuyer = new RandomBoxBuyer(300, 0); // 인스턴스 생성
+												// 현재잔액 포인트, 라이프 개수
+										
+					// 랜덤박스 구매: randomboxseller에게 원하는 값 지급
+					givePoint = 0;			
+					System.out.println("100p를 결제하세요. (랜덤박스는 1회당 1개씩 구매 가능합니다.)");	
+					givePoint = sc.nextInt();
+					if(givePoint%100==0) {
+						randomboxbuyer.buyRandomBox(randomboxseller, givePoint);
+					} else {
+						System.out.println("100원 단위로 입력해주세요. \n메뉴로 돌아갑니다.");
+					}
+					
+															
+					System.out.println("\n# 라이프 판매자의 현재 지표\n");
+					randomboxseller.showSaleRandomBoxResult();
+					
+					System.out.println("----------------------------------");
+					
+								
+					System.out.println("\n# 라이프 구매자의 현재 지표\n");
+					randomboxbuyer.showBuyRandomBoxResult();
+					
+					break;
+					
+				}
+			}
 		}
 
 
