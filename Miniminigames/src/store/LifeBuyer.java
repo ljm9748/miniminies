eab9a8faeac5f0ab767d7ed2b2ae732eea37f09d
JeongSuIn <suin9711@naver.com>
2020-11-02 19:00:02 +0900

    store
    store

---
+++ Miniminigames/src/store/LifeBuyer.java
@@ -0,0 +1,47 @@
+package store;
+
+public class LifeBuyer {
+
+	// 변수와 메서드 정의
+	
+		// 구매자의 소유 포인트 , 소유한 라이프 개수
+		private int myPoint; // 구매자의 보유 포인트
+		private int numOfLife; // 보유한 라이프 개수
+
+		// 기능: 라이프 구매, 현재 상태값(금액, 라이프의 개수)을 출력
+		// 라이프 구매
+		// 판매자정보(객체), 포인트를 받아
+		// 판매자의 판매 메소드 호출 -> 구매한 라이프의 개수를 얻는다.
+		// 나의 라이프의 개수를 증가
+		// 나의 보유 포인트를 감소
+		
+		// 생성자 정의 -> 변수의 초기화 목적
+		public LifeBuyer() {
+			this(300, 0); // 현재 잔액 포인트 , 라이프 개수 불러오기
+		}
+		
+		public LifeBuyer(int myPoint, int numOfLife) {
+			this.myPoint = myPoint; //this.인스턴스 변수 = 지역변수; / this -> 자신을 가리키는 참조변수
+			this.numOfLife = numOfLife;
+		}
+
+		// 참조변수는 객체의 주소를 저장하고 있다.-> 셀러의 참조값을 받도록 참조변수를 매개변수로 정의
+		// LifeSeller seller -> LifeSeller 클래스로 만든 객체의 주소를 저장하는 변수
+		void buyApple(LifeSeller seller, int point) {
+			// 구매할 사과의 개수를 구한다.
+			int numLife = seller.saleLife(point);
+			// 나의 라이프의 개수 증가
+			numOfLife += numLife;
+			// 나의 보유 포인트를 감소
+			myPoint -= point;
+		}
+
+		
+
+		// 현재 상태(금액, 라이프의 개수) 값을 출력
+		void showBuyLifeResult() {
+			System.out.println("보유 금액 >> " + myPoint);
+			System.out.println("보유 라이프 개수 >> " + numOfLife);
+		}
+	
+}
---
+++ Miniminigames/src/store/LifeSeller.java
@@ -0,0 +1,62 @@
+package store;
+
+public class LifeSeller {
+
+	
+	// 클래스에는 변수와 메소드를 정의
+		// 성격이 비슷한 변수, 메소드를 정의한다.		
+		
+		final int LIFE_PRICE; 	 				// 상수 처리 - 라이프의 가격, 변경이 불가능
+		private int numOfLife; 			 		// 라이프의 보유 개수
+		private int myPoint;				 	// 보유 포인트 // 잔액 불러오기
+		private int myMoney;					// 보유 금액
+		
+		
+		LifeSeller(){ // 기본 생성자
+			this(0, 20, 100); // 생성자 FruitSeller를 가리킴. // 라이프 보유 개수 무제한..?
+		}
+		
+		// 생성자의 오버로딩
+		LifeSeller(int point, int num, int price){
+			LIFE_PRICE=price;
+			numOfLife = num;
+			myPoint = point;
+		}
+		
+		LifeSeller(LifeSeller seller){		
+			this(seller.myPoint, seller.numOfLife, seller.LIFE_PRICE);
+		}
+		
+		
+		// 기능: 판매, 수익금을 정산 출력
+		// 판매 메소드:
+		//	돈을 받고  
+		// 		-> 반환하는 사과의 개수,
+		//		수익금 증가,
+		//		사과 보유 개수가 감소  
+		// 	-> 사과의 개수 반환
+		int saleLife(int point) {
+			
+			int numLife = 0; 		//반환할 라이프의 개수, 지역 변수는 반드시 초기화해주어야 한다. 
+			
+			// 반환할 사과의 개수를 구한다.
+			numLife = point/LIFE_PRICE; 		// 받은 돈/사과의 가격
+			// 수익금 증가
+			myPoint = myPoint + point;
+			// 사과 보유 개수 감소
+			numOfLife = numOfLife - numLife;
+			// money로 환전
+			myMoney = myPoint + point;
+			
+			return numLife;
+		}
+		
+		// 정산 출력: 보유한 사과의 개수 출력, 보유한 금액 출력, 
+		// 받는 데이터가 없으므로 매개변수 정의하지 않는다.
+		// 반환하는 데이터가 없으므로 반환타입을 void 타입으로 메소드 정의
+		void showSaleLifeResult() { 	// 반환 타입이 x
+			System.out.println("현재 보유한 사과의 개수: " + numOfLife);
+			System.out.println("판매 수익 금액: " + myPoint );
+			System.out.println("보유 금액 >> " + myMoney);
+		}
+}
