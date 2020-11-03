cf9e012dddde11706980dce15d216a8357047eec
uriguri <uriuriguriguri@gmail.com>
2020-11-03 10:36:02 +0900

    2020 11 03

--- Miniminigames/src/minigame2/Player.java
+++ Miniminigames/src/minigame2/Player.java
@@ -17,44 +17,43 @@ public class Player {
 	// 기본 Life는 5로 설정
 	int game2Life = 5;
 	
+	// contains를 활용하여 예외처리 함.
+	String drinkBase = "물 우유 커피 바닐라 초코 없음";
 
 	//플레이어는 음료를 만든다
-	public void playerMakeDrink() {
+	void playerMakeDrink() {
 		
 		// 계속 실행되도록 반복문 
 		while(game2Continue) {
 			
-		// 음료의 재료를 입력받을 스캐너
+		// 재료를 입력받을 스캐너
 		Scanner sc = new Scanner(System.in);
 		 
-		// 먼저 고객이 주문하는 것을 표현함.
+		// 먼저 고객이 주문하는내용을 보여줌.
 		Customer choiceMenu = new Customer();
 		choiceMenu.csOrder();
 		
-		System.out.println("**************************");
-		System.out.println("                  남은 목숨 : "+game2Life       );
+		System.out.println("************************************");
+		System.out.println("                  남은 기회 : "+game2Life       );
 		System.out.println("---------------------------------");
 		System.out.println(" ┌-----준비된 재료 리스트를 확인하세요----┐");
 		System.out.println(" | 물 | 우유 | 커피 | 바닐라 | 초코 | 없음|");
 		System.out.println(" └-------------------------------┘");
-		System.out.println("**************************");
+		System.out.println("************************************");
 		
-		// 메인재료(첫 번째 재료 스캐너로 입력받기)
-		System.out.println("메인 재료를 하나 선택해 입력해주세요([물] 또는 [우유])");
+	
+		System.out.println("첫 번째 재료를 입력해주세요([물],[우유],[커피],[바닐라],[초코],[없음])");
 		String var1 = sc.nextLine();
 		System.out.println("첫 번째로 넣을 재료는 = " + var1);
 		
-		// 메인재료 예외처리 
-		// -> 물이나 우유외에 다른내용이 입력되면
-		// -> 라이프를 차감하고 다시 선택하도록 함
-		// -> 라이프를 차감했을때 라이프가 0이라면 게임오버.
-		
-		if(var1.equals("물") || var1.equals("우유")){
-			System.out.println("메인 재료를 넣었습니다^0^");
+		// contains를 활용해서 해당하는 재료(drinkBase)가 아니면 else로 빠지게 함.
+		if(drinkBase.contains(var1)){
+			System.out.println("재료를 넣었습니다^0^");
 		} else {
 			--game2Life;
 			System.out.println("정해진 재료만 넣을 수 있습니다 T.T 남은목숨 : "+ game2Life);
 			
+		// 이 과정에서 game2Life가 0이면 그대로 게임오버.	
 			if(game2Life==0) {
 				game2Continue = false;
 				System.out.println("Game Over~~!");
@@ -62,14 +61,13 @@ public class Player {
 			continue;
 		}
 			
-		// 추가재료 선택하기 (메인재료 예외처리와 동일)
-		System.out.println("추가재료를 입력해주세요([커피],[바닐라],[초코],[없음])");
+
+		System.out.println("두 번째 재료를 입력해주세요([물],[우유],[커피],[바닐라],[초코],[없음])");
 		String var2 = sc.nextLine();
-		System.out.println("두 번째 선택한 재료는 = " + var2);
+		System.out.println("두 번째로 넣을 재료는 = " + var2);
 		
-		// 추가재료 예외처리
-		if(var2.equals("커피") || var2.equals("바닐라") || var2.equals("초코")||var2.equals("없음")){
-			System.out.println("추가 재료 1번을 넣었습니다^0^");
+		if(drinkBase.contains(var2)){
+			System.out.println("재료를 넣었습니다^0^");
 		} else {
 			--game2Life;
 			System.out.println("정해진 재료만 넣을 수 있습니다 T.T 남은목숨 : "+ game2Life);
@@ -81,14 +79,13 @@ public class Player {
 			continue;
 		}
 		
-		// 추가재료 선택하기2
-		System.out.println("추가재료를 입력해주세요([커피],[바닐라],[초코],[없음])");
-		String var3 = sc.nextLine();
-		System.out.println("세 번째 선택한 재료는 = " +var3);
 		
-		// 추가재료 예외처리2
-		if(var3.equals("커피") || var3.equals("바닐라") || var3.equals("초코")||var3.equals("없음")){
-			System.out.println("추가 2번을 넣었습니다^0^");
+		System.out.println("세 번째 재료를 입력해주세요([물],[우유],[커피],[바닐라],[초코],[없음])");
+		String var3 = sc.nextLine();
+		System.out.println("세 번째로 넣을 재료는 = " +var3);
+		
+		if(drinkBase.contains(var3)){
+			System.out.println("재료를 넣었습니다^0^");
 		} else {
 			--game2Life;
 			System.out.println("정해진 재료만 넣을 수 있습니다 T.T 남은목숨 : "+ game2Life);
@@ -101,22 +98,21 @@ public class Player {
 		}
 		
 		// 음료 제공 (비교 및 판단하기)
-		
-		// 플레이어가 입력한 var1, var2, var3이 recipe에 들어가 음료명(drink) 로 반환됨. 
+		// 플레이어가 입력한 var1, var2, var3이 MakeDrink.recipe 에 들어가 음료명(drink)로 반환됨. 
 		MakeDrink playerMake = new MakeDrink();
 		playerMake.recipe(var1, var2, var3);
 		
-		// 플레이어가 만든 음료 = recipe에 입력되어 반환된 음료이름 
+		// 플레이어가 만든 음료 = recipe에 입력되어 반환된 음료이름 (gtter사용)
 		String pDrink = playerMake.getDrink();
 		
-		// 고객이 주문한 음료 = csOrder 에서 선택된 메뉴 (return의 값 : menu)
+		// 고객이 주문한 음료
 		String cDrink = choiceMenu.lastChoice();
-		
 		
 		// 고객이 주문한음료 .equals 플레이거가 만든음료  
 		if(cDrink.equals(pDrink)) {
-			System.out.println("잘 마셨습니다~ ^^ 포인트 +100"); // 포인트는 팀원들과 협의 후 적정선으로 int타입으로 추가 (10:45)
+			System.out.println("잘 마셨습니다~ ^^ 포인트 +50"); // 포인트 100단위로 수정해야함 (1103 10:16)
 			break;
+			
 		// 고객이 주문한 음료와 일치하지 않는다면? =  라이프 차감 + 포인트없음 + 실패	
 		} else {
 			--game2Life;
--- Miniminigames/src/minigame2/hardMode.java
+++ Miniminigames/src/minigame2/hardMode.java
@@ -14,71 +14,79 @@ public class hardMode {
 	
 	// 기본 nomalMode는 Life는 3으로 설정
 	// 모두 맞춰야 점수를 받기때문에 한 번만 실패해도 탈락
-	int game2Life = 3;
+	private int game2Life = 3;
 	
 	// nomalMode는 3번을 모두 성공해야 포인트를 받을 수 있음.
-	int victory = 0;
+	// 성공시 ++victory를 해주어서 3이되면 성공
+	private int victory = 0;
 	
-	//플레이어는 음료를 만든다
+	// contains를 활용하여 예외처리 함.
+		String drinkBase = "물 우유 커피 바닐라 초코 없음";
+	// 게임 랭킹에 사용 될 승,패
+	private int win = 0;
+	private int lose = 0;
+	
 	void playerMakeDrink() {
 		
 	// 계속 실행되도록 반복문 
 	while(game2Continue) {
 		
-		// 만약 victory == 3이면 포인트를 얻는다! -> 게임종료(성공)
+		// 만약 victory == 3이면 포인트를 얻는다!
 		if(victory == 3) {
-			System.out.println("대 성공~!! point+200");
+			++win;
+			System.out.println("대 성공~!! point+200"); // 포인트 처리해야함.
 			break;
 		}
 			
 		// 음료의 재료를 입력받을 스캐너
 		Scanner sc = new Scanner(System.in);
 		
-		// 먼저 고객이 주문하는 것을 표현함.
+		// 고객이 주문하는 것을 출력
 		Customer choiceMenu = new Customer();
 		choiceMenu.csOrder();
 		
-		System.out.println("**************************");
+		System.out.println("하드모드는 세번 연속 성공해야 포인트를 얻을 수 있습니다.");
+		System.out.println("************************************");
 		System.out.println(" 한  번  틀  리  면  게  임  오  버  입  니  다" );
 		System.out.println("---------------------------------");
 		System.out.println(" ┌-----준비된 재료 리스트를 확인하세요----┐");
 		System.out.println(" | 물 | 우유 | 커피 | 바닐라 | 초코 | 없음|");
 		System.out.println(" └-------------------------------┘");
-		System.out.println("**************************");
+		System.out.println("************************************");
 		
-		// 메인재료(첫 번째 재료 스캐너로 입력받기)
-		System.out.println("메인 재료를 하나 선택해 입력해주세요([물] 또는 [우유])");
+		System.out.println("첫 번째 재료를 입력해주세요([물],[우유],[커피],[바닐라],[초코],[없음])");
 		String var1 = sc.nextLine();
 		System.out.println("첫 번째로 넣을 재료는 = " + var1);
 		
-		// 메인재료 예외처리 
-		// -> 물이나 우유외에 다른내용이 입력되면
-		// -> 라이프를 차감하고 다시 선택하도록 함
-		// -> 라이프를 차감했을때 라이프가 0이라면 게임오버.
-		if(var1.equals("물") || var1.equals("우유")){
-			System.out.println("메인 재료를 넣었습니다^0^");
+		// contains를 활용해서 해당하는 재료(drinkBase)가 아니면 else로 빠지게 함.
+		if(drinkBase.contains(var1)){
+			++victory;
+			System.out.println("재료를 넣었습니다^0^");
 		} else {
 			--game2Life;
+			++lose;
 			System.out.println("정해진 재료만 넣을 수 있습니다 T.T");
 			
-			if(game2Life < 3 ){
+		// 이 과정에서 game2Life가 < 3이면 그대로 게임오버.	
+			if(game2Life < 3) {
 				game2Continue = false;
 				System.out.println("Game Over~~!");
 			}
 			continue;
 		}
 			
-		// 추가재료 선택하기 (메인재료 예외처리와 동일)
-		System.out.println("추가재료를 입력해주세요([커피],[바닐라],[초코],[없음])");
+
+		System.out.println("두 번째 재료를 입력해주세요([물],[우유],[커피],[바닐라],[초코],[없음])");
 		String var2 = sc.nextLine();
-		System.out.println("두 번째 선택한 재료는 = " + var2);
+		System.out.println("두 번째로 넣을 재료는 = " + var2);
 		
-		// 추가재료 예외처리
-		if(var2.equals("커피") || var2.equals("바닐라") || var2.equals("초코")||var2.equals("없음")){
-			System.out.println("추가 재료 1번을 넣었습니다^0^");
+		if(drinkBase.contains(var2)){
+			++victory;
+			System.out.println("재료를 넣었습니다^0^");
 		} else {
 			--game2Life;
-			System.out.println("정해진 재료만 넣을 수 있습니다 T.T ");
+			++lose;
+			System.out.println("정해진 재료만 넣을 수 있습니다 T.T");
 			
 			if(game2Life < 3) {
 				game2Continue = false;
@@ -87,16 +95,17 @@ public class hardMode {
 			continue;
 		}
 		
-		// 추가재료 선택하기2
-		System.out.println("추가재료를 입력해주세요([커피],[바닐라],[초코],[없음])");
-		String var3 = sc.nextLine();
-		System.out.println("세 번째 선택한 재료는 = " +var3);
 		
-		// 추가재료 예외처리2
-		if(var3.equals("커피") || var3.equals("바닐라") || var3.equals("초코")||var3.equals("없음")){
-			System.out.println("추가 2번을 넣었습니다^0^");
+		System.out.println("세 번째 재료를 입력해주세요([물],[우유],[커피],[바닐라],[초코],[없음])");
+		String var3 = sc.nextLine();
+		System.out.println("세 번째로 넣을 재료는 = " +var3);
+		
+		if(drinkBase.contains(var3)){
+			++victory;
+			System.out.println("재료를 넣었습니다^0^");
 		} else {
 			--game2Life;
+			++lose;
 			System.out.println("정해진 재료만 넣을 수 있습니다 T.T");
 			
 			if(game2Life < 3) {
@@ -122,9 +131,10 @@ public class hardMode {
 		// 고객이 주문한음료 .equals 플레이어가 만든음료
 		if(cDrink.equals(pDrink)) {
 			++victory;
-			System.out.println("잘 마셨습니다~ ^^ 성공 회수 : " + victory); 			
+			System.out.println("잘 마셨습니다~ ^^ 성공 횟수 : " + victory); 			
 		} else {
 			--game2Life;
+			++lose;
 			System.out.println("더럽게 맛 없네 다신 안와!!!!!!!!");
 			
 			if(game2Life < 3) {
