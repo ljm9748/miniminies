27db85f75867981dcf0b2a796e92b980debd73f1
choejh <choeejihye@gmail.com>
2020-11-04 13:02:55 +0900

    1104

--- Miniminigames/src/minigame3/Level1CCGame.java
+++ Miniminigames/src/minigame3/Level1CCGame.java
@@ -21,7 +21,7 @@ public class Level1CCGame  extends CCGame{
 		System.out.println("         예를 들어, 12+3= 라는 문제가 나오면 '15'를 입력하시면 됩니다.    ");
 		System.out.println("         총 5문제 중에 3문제를 맞추면 통과! 3문제 이상을 틀리면 탈락합니다!   ");
 		System.out.println("============================================================");
-		System.out.println("메인메뉴로 돌아간다-1 시작한다-2");
+		System.out.println("1.게임시작 \n2.난이도 선택으로 돌아가기");
 		
 		try {
 		select= sc.nextInt();
@@ -36,17 +36,18 @@ public class Level1CCGame  extends CCGame{
 		}
 		
 		switch(select) {
-		case 1 : return;
-		case 2 :
-			members.useLife();
-			System.out.println("게임을 시작합니다.");
-		
+		case 1 : 
+				members.useLife();
+				System.out.println("게임을 시작합니다.");
 				Over o = new Over();
 				Lv1Start lv1 = new Lv1Start(o);
 				CountDownThread cdt = null;
 				cdt = new CountDownThread(o);
 				cdt.start();
 				lv1.playGame();
+			
+		case 2 : return;
+
 			
 				
 		}
